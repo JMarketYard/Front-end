5c698ad6d2368649f33661f4b7331539c7763ef8
devhaeun <lala819010@gmail.com>
2025-01-27 02:28:21 +0900

    Feat: HeaderIconMenu 컴포넌트 추가 #13
    태블릿/휴대폰 화면에 있는 둥근 아이콘 메뉴를 HeaderIconMenu 컴포넌트로 분리함.

--- src/layout/RootLayout.tsx
+++ src/layout/RootLayout.tsx
@@ -1,11 +1,13 @@
 import styled from "styled-components";
 import Header from "../components/Header";
 import { Outlet } from "react-router-dom";
+import HeaderIconMenu from "../components/HeaderIconMenu";
 
 const RootLayout = () => {
     return (
         <Wrapper>
             <Header />
+            <HeaderIconMenu />
             <Outlet />
         </Wrapper>
     );
@@ -16,5 +18,6 @@ export default RootLayout;
 const Wrapper = styled.div`
     // min-width: 1440px;
     display: flex;
+    flex-direction: column;
     justify-content: center;
 `
