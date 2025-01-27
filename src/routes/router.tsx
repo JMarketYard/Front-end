83f05999b6e76b7d42bc999eb2d74aa8d60baf41
김예린 <lynn1833@ewhain.net>
2025-01-28 01:20:57 +0900

    추가 세팅 #15

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import Login from '../pages/login/login';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import ModalProvider from '../components/Modal/context/ModalProvider';
 
 const router = createBrowserRouter([
   {
