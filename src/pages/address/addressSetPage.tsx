191981961f6b0db2f97bd8053713a8e117cc64f8
devhaeun <lala819010@gmail.com>
2025-01-29 02:53:55 +0900

    Design: 배송지 선택 버튼 UI #17
    배송지 설정 선택/선택 취소 버튼 UI 구현

--- src/pages/address/addressSetPage.tsx
+++ src/pages/address/addressSetPage.tsx
@@ -1,11 +1,28 @@
 import styled from "styled-components";
 import BigTitle from "../../components/BigTitle";
 import Address from "./components/Address";
+import { useState } from "react";
 
 const AddressSetPage = () => {
+  const [isSelect, setIsSelect] = useState(false);
+
   return (
     <Wrapper>
-      <BigTitle>배송지 설정</BigTitle>
+      <BigTitle>
+        배송지 설정
+        {isSelect
+        ? <SelectBtn onClick={()=>setIsSelect(false)}>선택</SelectBtn>
+        : <>
+        <SelectBtn
+        $right={'128px'}
+        >배송지 삭제</SelectBtn>
+        <SelectBtn
+        $background={'rgba(201, 8, 255, 0.20)'}
+        color={'#C908FF'}
+        onClick={()=>setIsSelect(true)}
+        >선택 취소</SelectBtn>
+        </>}
+      </BigTitle>
       <AddressList>
         <Address />
       </AddressList>
@@ -21,6 +38,31 @@ const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+`
+
+const SelectBtn = styled.button<{$background?:string, color?:string, $right?:string}>`
+  display: inline-flex;
+  height: 31px;
+  padding: 0px 14px;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  border-radius: 11px;
+  border: 1px solid #C908FF;
+  background: ${props => props.$background ? props.$background : '#C908FF'};
+  color: ${props => props.color ? props.color : '#FFF'};
+  text-align: center;
+  font-family: Pretendard;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 36.832px; /* 204.622% */
+  position: absolute;
+  right: ${props => props.$right ? props.$right : '0'};
+  box-sizing: border-box;
+  &:hover {
+    cursor: pointer;
+  };
 `
 
 const AddressList = styled.ul`
