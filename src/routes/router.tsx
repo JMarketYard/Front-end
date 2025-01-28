2f1def2def7a7dd9869e215e2323a962a2be0ee5
김예린 <lynn1833@ewhain.net>
2025-01-28 18:21:56 +0900

    feat/로그인 모달 연결 #18

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -2,13 +2,29 @@ import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import RootLayout from '../layout/RootLayout';
+import ChargePage from '../pages/charge/chargePage';
+import ModalProvider from '../components/Modal/context/ModalProvider';
 
 const router = createBrowserRouter([
   {
     path: '/',
-
-    element: <RootLayout />,
-    children: [],
+    element: (
+      <div>
+        <RootLayout />
+        <ModalProvider />
+      </div>
+    ),
+    children: [
+      {
+        path: 'change',
+        element: (
+          <div>
+            <ChargePage />
+            <ModalProvider />
+          </div>
+        ),
+      },
+    ],
   },
 ]);
 
