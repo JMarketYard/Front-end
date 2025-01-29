f45a4558d46a52dda3b7e072397bfeeacd5293a9
devhaeun <lala819010@gmail.com>
2025-01-29 21:13:42 +0900

    Feat: 후기 남기기 페이지 UI 개발 #23
    후기 남기기 페이지 개발: 평점 남기기 별 컴포넌트 외에 완성

---
+++ src/pages/writeReview/writeReview.tsx
@@ -0,0 +1,68 @@
+import styled from "styled-components";
+import BigTitle from "../../components/BigTitle";
+
+const WriteReview = () => {
+  return (
+    <>
+    <Wrapper>
+      <BigTitle>후기 남기기</BigTitle>
+      <ScoreBox>
+        평점 매기기
+      </ScoreBox>
+      <Textarea placeholder="후기는 1~100자로 입력해주세요." />
+      <Button>작성 완료</Button>
+    </Wrapper>
+    </>
+  );
+};
+
+export default WriteReview;
+
+const Wrapper = styled.form`
+  padding-top: 63px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const ScoreBox = styled.div`
+  color: #8F8E94;
+  text-align: center;
+  font-family: Pretendard;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+  padding-top: 73px;
+  margin-bottom: 65px;
+`
+
+const Textarea = styled.textarea`
+  width: 866px;
+  height: 497px;
+  padding: 10px;
+  margin-bottom: 137px;
+  border: 1px solid #8F8E94;
+  font-family: Pretendard;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`
+
+const Button = styled.button`
+  width: 424px;
+  height: 57px;
+  border-radius: 7px;
+  border: none;
+  background: #C908FF;
+  color: #FFF;
+  text-align: center;
+  font-family: Pretendard;
+  font-size: 22px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 18px; /* 81.818% */
+  letter-spacing: -0.165px;
+  margin-bottom: 167px;
+`
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -5,6 +5,7 @@ import RootLayout from '../layout/RootLayout';
 import ChargePage from '../pages/charge/chargePage';
 import ModalProvider from '../components/Modal/context/ModalProvider';
 import RaffleUploadPage from '../pages/RaffleUploadPage';
+import WriteReview from '../pages/writeReview/writeReview';
 
 const router = createBrowserRouter([
   {
@@ -28,6 +29,10 @@ const router = createBrowserRouter([
         {
         path: 'raffle-upload',
         element: <RaffleUploadPage />
+      },
+      {
+        path: 'review',
+        element: <WriteReview />
       }
     ],
   },
