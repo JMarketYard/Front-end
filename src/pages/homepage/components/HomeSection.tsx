c1bbce35d95a1c2011e75e8f27c01b3fef1ab75b
jade <kmj9151@naver.com>
2025-02-04 15:35:55 +0900

    Refactor: ImminentDeadline, MyLike, MyFollow를 HomeSection 컴포넌트로 통합. Feat: API 연결 준비 (백 서버 연결 전)

--- src/pages/homepage/components/HomeSection.tsx
+++ src/pages/homepage/components/HomeSection.tsx
@@ -1,36 +1,62 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
-import like from '../../../assets/homePage/like.svg';
 import moreList from '../../../assets/homePage/moreList.svg';
 import SmallProductCard from '../../../components/SmallProductCard';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
-const Mylike = () => {
+interface Product {
+  raffleId: number;
+  imageUrl: string;
+  name: string;
+  ticketNum: number;
+  timeUntilEnd: number;
+  finish: boolean;
+  participantNum: number;
+  like: boolean;
+}
+
+interface HomeSectionProps {
+  title: string;
+  icon: string;
+  moreText: string;
+  apiKey: 'approaching' | 'myLikeRaffles' | 'myFollowRaffles';
+  moreLink: string;
+  products: Product[];
+}
+
+const HomeSection: React.FC<HomeSectionProps> = ({
+  title,
+  icon,
+  moreText,
+  apiKey,
+  moreLink,
+  products,
+}) => {
   const navigate = useNavigate();
-  const products: null[] = Array(5).fill(null);
 
   return (
     <Wrapper>
       <HeaderContainer>
         <TextBox>
-          <img src={like} alt="like" /> 내가 찜한 래플
+          <img src={icon} alt="icon" /> {title}
         </TextBox>
-        <MoreListBox onClick={() => navigate('/')}>
-          내가 찜한 래플 더보기
+        <MoreListBox onClick={() => navigate(moreLink)}>
+          {moreText}
           <img src={moreList} alt="moreList" />
         </MoreListBox>
       </HeaderContainer>
 
-      <ProductGrid>
-        {products.map((_, index) => (
-          <SmallProductCard key={index} />
+      <ProductContainer>
+        {products.map((product) => (
+          <SmallProductCard key={product.raffleId} {...product} />
         ))}
-      </ProductGrid>
+      </ProductContainer>
     </Wrapper>
   );
 };
 
-export default Mylike;
+export default HomeSection;
 
 const Wrapper = styled.div`
   width: 100%;
@@ -42,59 +68,39 @@ const HeaderContainer = styled.div`
   display: flex;
   height: 38px;
   margin-bottom: 11px;
-  flex-direction: row;
   justify-content: space-between;
-  flex-shrink: 0;
-
   align-items: center;
 `;
 
 const TextBox = styled.div`
   display: flex;
   gap: 23px;
-
   color: #000;
   font-family: Inter;
   font-size: 22px;
-  font-style: normal;
   font-weight: 600;
-  line-height: normal;
 `;
 
 const MoreListBox = styled.a`
-  width: 220px;
+  width: 240px;
   height: 34px;
   display: flex;
   justify-content: flex-end;
   align-items: center;
-  display: flex;
   color: #8f8e94;
-  text-align: center;
-  font-family: Pretendard;
   font-size: 16px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: 36.832px; /* 230.199% */
-  text-decoration-line: underline;
-  text-decoration-style: solid;
-  text-decoration-skip-ink: auto;
-  text-decoration-thickness: auto;
-  text-underline-offset: auto;
-  text-underline-position: from-font;
+  text-decoration: underline;
+  cursor: pointer;
 
   img {
     width: 10px;
     height: 17px;
     margin-left: 35px;
   }
-
-  cursor: pointer;
 `;
 
-const ProductGrid = styled.div`
-  display: grid;
-  grid-template-columns: repeat(5, 1fr);
-  place-items: center;
-  width: 100%;
-  max-width: 1080px;
+const ProductContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
 `;
