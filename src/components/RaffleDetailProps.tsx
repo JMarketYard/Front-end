abc3bc785a466c081312814067d1ce37d700bd52
jade <kmj9151@naver.com>
2025-02-09 20:12:04 +0900

    Feat: RaffleDetailProps undefined 해결

--- src/components/RaffleDetailProps.tsx
+++ src/components/RaffleDetailProps.tsx
@@ -1,21 +1,21 @@
 interface RaffleDetailProps {
-  imageUrls: string[];
-  name: string;
-  category: string;
-  ticketNum: number;
-  startAt: string; // ISO 8601 형식의 날짜 (Date 변환 가능)
-  endAt: string;
-  description: string;
-  minUser: number;
-  view: number;
-  likeCount: number;
-  applyCount: number;
-  nickname: string;
-  followCount: number;
-  reviewCount: number;
-  userStatus: string;
-  isWinner: string;
-  raffleStatus: string;
+  imageUrls?: string[];
+  name?: string;
+  category?: string;
+  ticketNum?: number;
+  startAt?: string; // ISO 8601 형식의 날짜 (Date 변환 가능)
+  endAt?: string;
+  description?: string;
+  minUser?: number;
+  view?: number;
+  likeCount?: number;
+  applyCount?: number;
+  nickname?: string;
+  followCount?: number;
+  reviewCount?: number;
+  userStatus?: string;
+  isWinner?: string;
+  raffleStatus?: string;
   children?: React.ReactNode;
 }
 
