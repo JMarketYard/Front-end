86abbdcb976d1ced1f381960f2bf2c8e973a9ba3
jade <kmj9151@naver.com>
2025-02-15 11:36:41 +0900

    Design: 래플 상태에 따른 문구(도넛그래프 위치) 크기 조정

--- src/pages/raffleDetail/components/Probability.tsx
+++ src/pages/raffleDetail/components/Probability.tsx
@@ -88,7 +88,7 @@ const Probability: React.FC<RaffleDetailProps> = (raffle) => {
       {raffle.raffleStatus === 'CANCELLED' && (
         <>
           {raffle.applyCount < raffle.minUser && ( //미달 취소
-            <SmallContainer>
+            <FailedContainer>
               래플 종료
               <FailedBox>
                 해당 래플은 판매자 희망 최소 참여자
@@ -99,7 +99,7 @@ const Probability: React.FC<RaffleDetailProps> = (raffle) => {
                 <br />
                 적립됩니다.
               </FailedBox>
-            </SmallContainer>
+            </FailedContainer>
           )}
           {raffle.applyCount >= raffle.minUser && ( //ENDED에서 배송/운송 관련으로 취소, 추후 멘트 필요.
             <SmallContainer>
@@ -220,7 +220,7 @@ const ParticipantBox = styled.div`
 
 const FailedContainer = styled.div`
   width: 285px;
-  height: 185px;
+  height: 195px;
   flex-direction: column;
   justify-content: center;
   align-items: center;
