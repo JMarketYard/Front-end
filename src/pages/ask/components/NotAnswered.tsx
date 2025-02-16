fc104181beaf73b71ba91d4fc97cca9939613fcf
devhaeun <lala819010@gmail.com>
2025-02-16 22:35:07 +0900

    Feat: 문의 페이지 기본 UI 개발 #80
    문의 게시판 기본 UI 및 탭 메뉴 전환 구현

---
+++ src/pages/ask/components/NotAnswered.tsx
@@ -0,0 +1,7 @@
+const NotAnswered = () => {
+  return (
+    <div>답변 미작성 탭</div>
+  )
+}
+
+export default NotAnswered;
---
+++ src/pages/ask/components/WriteAsk.tsx
@@ -0,0 +1,7 @@
+const WriteAsk = () => {
+  return (
+    <div>문의하기 탭</div>
+  )
+}
+
+export default WriteAsk;
