2b62a719098f07c9b723b34d7dc59b7f7f8bbd10
urimida <143990075+urimida@users.noreply.github.com>
2025-02-21 02:37:52 +0900

    Feat/mypage 54 (#125)
    * 상대프로필 보기
    
    * 제리가 잘못한거 빼고 다함
    
    * 마이페이지 관련 모든 api 연결 끝
    
    * 마이페이지, 상대페이지 반응형
    
    * 결제페이지 오류 해결
    
    * 팔로우 오류 해결
    
    * 마이페이지와 상대프로필 반응형
    
    * 마이페이지와 상대프로필 반응형2
    
    * 마이페이지와 상대프로필 반응형3
    
    * 마이페이지와 상대프로필 반응형4

--- src/components/BigTitle.tsx
+++ src/components/BigTitle.tsx
@@ -2,44 +2,46 @@ import { ReactNode } from "react";
 import styled from "styled-components";
 import media from "../styles/media";
 
-const BigTitle = ({children}:{children:ReactNode}) => {
-    return (
-        <BigTitleBox>
-            <TitleIcon />
-            {children}
-        </BigTitleBox>
-    )
-}
+const BigTitle = ({ children }: { children: ReactNode }) => {
+  return (
+    <BigTitleBox>
+      <TitleIcon />
+      {children}
+    </BigTitleBox>
+  );
+};
 
 export default BigTitle;
 
 const BigTitleBox = styled.div`
-    width: 1080px;
-    height: 53px;
-    display: flex;
-    align-items: center;
-    border-bottom: 1px solid black;
-    color: #000;
-    font-family: Pretendard;
-    font-size: 22px;
-    font-style: normal;
-    font-weight: 600;
-    line-height: normal;
-    padding-bottom: 24px;
-    box-sizing: border-box;
-    position: relative;
-    
-    ${media.medium`
-        width: 675px;
-        margin: 0 14px;
-    `}
-`
+  width: 1080px;
+  height: 53px;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid black;
+  color: #000;
+  font-family: Pretendard;
+  font-size: 22px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+  padding-bottom: 24px;
+  box-sizing: border-box;
+  position: relative;
+  margin-left : 10px;
+  margin-right : 10px;
+
+  ${media.medium`
+      width: 675px;
+      margin: 0 14px;
+  `}
+`;
 
 const TitleIcon = styled.span`
-    display: inline-block;
-    width: 14px;
-    height: 14px;
-    background-color: #C908FF;
-    border-radius: 50%;
-    margin-right: 52px;
-`
+  display: inline-block;
+  width: 14px;
+  height: 14px;
+  background-color: #C908FF;
+  border-radius: 50%;
+  margin-right: 52px;
+`;
