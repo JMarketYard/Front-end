b99addf7d9ad05fe5e5707367e31bf2a4a240cbf
urimida <crystal5k123@gmail.com>
2025-02-21 02:36:32 +0900

    마이페이지와 상대프로필 반응형4

--- src/components/SmallTitle.tsx
+++ src/components/SmallTitle.tsx
@@ -17,29 +17,43 @@ const SmallTitle: React.FC<SmallTitleProps> = ({ children, onClick }) => {
 
 export default SmallTitle;
 
-/* 스타일 */
 const SmallTitleBox = styled.div`
-  width: 200px;
-  height: 21px;
-  display: flex;
-  align-items: center;
-  color: #000;
-  font-family: Pretendard;
-  font-size: 20px;
-  font-style: normal;
-  font-weight: 600;
-  position: relative;
+width: fit-content;
+min-width: 150px; /* ✅ 최소 너비 설정 */
+height: 21px;
+display: flex;
+align-items: center;
+color: #000;
+font-family: Pretendard;
+font-size: 20px;
+font-style: normal;
+font-weight: 600;
+position: relative;
+transition: font-size 0.3s ease, margin 0.3s ease;
 
-  &:hover {
-    color: #c908ff;
-  }
+&:hover {
+  color: #c908ff;
+}
 `;
 
 const SmallTitleIcon = styled.span`
-  display: inline-block;
-  width: 14px;
-  height: 14px;
-  background-color: rgba(201, 8, 255, 0.2);
-  border-radius: 50%;
-  margin-right: 52px;
+display: inline-block;
+width: 14px;
+height: 14px;
+background-color: rgba(201, 8, 255, 0.2);
+border-radius: 50%;
+margin-right: 52px;
+transition: margin 0.3s ease;
+
+@media (max-width: 1024px) { /* 태블릿 */
+  margin-right: 40px;
+}
+
+@media (max-width: 768px) { /* 스마트폰 가로 */
+  margin-right: 30px;
+}
+
+@media (max-width: 480px) { /* 스마트폰 세로 */
+  margin-right: 20px;
+}
 `;
