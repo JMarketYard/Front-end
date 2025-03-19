9132e7838b71d8dd840fc2f4fc007d6c2e315499
devhaeun <lala819010@gmail.com>
2025-03-19 18:56:07 +0900

    Chore: PrivateRoute 내 location 삭제
    현재 로그인 페이지가 아닌 모달을 이용하고 있어서, 로그인 시 곧바로 이동 중이던 페이지로 리다이렉트 불가능하여 location 변수 삭제

--- src/services/PrivateRoute.tsx
+++ src/services/PrivateRoute.tsx
@@ -3,15 +3,12 @@ import { useAuth } from "../context/AuthContext"
 
 const PrivateRoute = () => {
   const {isAuthenticated} = useAuth();
-  
-  // 현재 URL 확인
-  const location = useLocation();
 
   if (!isAuthenticated) {
     alert("개인회원 로그인 후 이용해주세요.");
     // ✅ 메인 페이지로 이동
     // ❎ 로그인 모달 열기
-    return <Navigate to="/" state={{ from: location }} />;
+    return <Navigate to="/" />;
   }
 
   // 로그인 상태라면 원래 컴포넌트 렌더링
