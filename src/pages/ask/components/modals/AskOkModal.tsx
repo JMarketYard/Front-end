ad72c3ae79ae883e11f85133d8c868930a2553c0
devhaeun <lala819010@gmail.com>
2025-04-02 03:24:35 +0900

    Chore: 모달 폴더 이동

--- src/pages/ask/components/modals/AskOkModal.tsx
+++ src/pages/ask/components/modals/AskOkModal.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import Modal from '../Modal';
+import Modal from '../../../../components/Modal/Modal';
 import smileVector from '../../../assets/SmileVector.png';
 
 interface ModalProps {
