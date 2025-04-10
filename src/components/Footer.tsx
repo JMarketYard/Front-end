e1619aa7d6c6d43103179ff181f8778b015e1910
jade <kmj9151@naver.com>
2025-04-10 16:22:52 +0900

    Design : Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -13,28 +13,28 @@ const Footer: React.FC = () => {
   return (
     <FooterContainer>
       <FooterContent>
-      <Menu>
-        {menuItems.map((item, index) => (
-          <MenuItem
-            key={index}
-            isLast={index === menuItems.length - 1}
-            isFirst={index === 0} // 첫번째 메뉴만 패딩 없게
-          >
-            {item}
-          </MenuItem>
-        ))}
-      </Menu>
+        <Menu>
+          {menuItems.map((item, index) => (
+            <MenuItem
+              key={index}
+              isLast={index === menuItems.length - 1}
+              isFirst={index === 0} // 첫번째 메뉴만 패딩 없게
+            >
+              {item}
+            </MenuItem>
+          ))}
+        </Menu>
 
         <InfoSection>
           <InfoBlock>
             <BoldText>장마당(주) 사업자정보</BoldText>
-            <Line>대표 : 송유림 ㅣ 사업자등록번호 : 000-00-00000</Line>
+            <Line>대표 : 송유림 ㅣ 사업자등록번호 : 307-43-01173</Line>
             <Line>통신판매업신고 : 0000-경기김포-0000</Line>
             <Line>호스팅서비스 제공자 : Amazon Web Services (AWS)</Line>
           </InfoBlock>
           <InfoBlock>
             <Spacer />
-            <Line>EMAIL : 000@jangmadang.com ㅣ FAX : 000-0000-0000</Line>
+            <Line>EMAIL : jangmadang2025@gmail.com ㅣ FAX : 000-0000-0000</Line>
             <Line>주소 : 경기도 김포시 김포한강9로75번길 66</Line>
             <Line>대표전화 : 010-7689-6108</Line>
           </InfoBlock>
@@ -51,10 +51,14 @@ const Footer: React.FC = () => {
         <FooterBottom>
           <Notice>
             <Line>
-              장마당은 온라인 거래를 중개하는 플랫폼으로서, 직접 상품을 판매하는 사업자가 아닙니다. 따라서, 전자상거래 등에서의 소비자 보호에 관한 법률 및 장마당의 이용 약관에 따라,
+              장마당은 온라인 거래를 중개하는 플랫폼으로서, 직접 상품을 판매하는
+              사업자가 아닙니다. 따라서, 전자상거래 등에서의 소비자 보호에 관한
+              법률 및 장마당의 이용 약관에 따라,
             </Line>
             <Line>
-              상품의 등록, 정보 제공, 거래 및 그로 인한 책임은 모두 개별 판매자에게 귀속됩니다. 장마당은 회원 간 이루어지는 거래에 대해 직접적인 책임을 부담하지 않으며, 거래 과정에서
+              상품의 등록, 정보 제공, 거래 및 그로 인한 책임은 모두 개별
+              판매자에게 귀속됩니다. 장마당은 회원 간 이루어지는 거래에 대해
+              직접적인 책임을 부담하지 않으며, 거래 과정에서
             </Line>
             <Line>
               발생하는 사항은 해당 판매자와 구매자 간의 계약에 따라 처리됩니다.
@@ -90,7 +94,7 @@ const FooterContent = styled.div`
   display: flex;
   flex-direction: column;
   padding: 32px 24px;
-  color: #8F8E94;
+  color: #8f8e94;
   font-size: 13px;
   box-sizing: border-box;
 `;
@@ -107,7 +111,7 @@ const MenuItem = styled.li<{ isLast: boolean }>`
   margin-left: ${({ isFirst }) => (isFirst ? '0' : '120px')};
   font-weight: 400;
   cursor: pointer;
-  color: #8F8E94;
+  color: #8f8e94;
   display: flex;
   align-items: center;
 
@@ -119,7 +123,7 @@ const MenuItem = styled.li<{ isLast: boolean }>`
     content: ${({ isLast }) => (isLast ? "''" : "'|'")};
     position: absolute;
     right: -65px;
-    color: #8F8E94;
+    color: #8f8e94;
   }
 `;
 
@@ -145,7 +149,7 @@ const CustomerCenter = styled.div`
 const CenterTitle = styled.div`
   font-size: 18px;
   font-weight: 500;
-  color: #8F8E94;
+  color: #8f8e94;
   margin-bottom: 8px;
   letter-spacing: -0.3px;
 `;
@@ -153,13 +157,13 @@ const CenterTitle = styled.div`
 const PhoneText = styled.div`
   font-size: 25px;
   font-weight: 700;
-  color: #8F8E94;
+  color: #8f8e94;
   line-height: 1.3;
   letter-spacing: -0.5px;
 `;
 
 const BoldText = styled.div`
-  color: #8F8E94;
+  color: #8f8e94;
   font-size: 25px;
   font-style: normal;
   font-weight: 700;
@@ -198,7 +202,7 @@ const FooterBottom = styled.div`
 
 const Notice = styled.div`
   font-size: 12px;
-  color: #8F8E94;
+  color: #8f8e94;
   line-height: 1.6;
   margin-bottom: 16px;
 `;
